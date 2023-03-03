215c3ef0950f039e17faa74cd2e31b7a1974f988
Milo Bonnet <milovtt@gmail.com>
2023-03-03 10:28:48 +0100

    Create route & view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
